b199d3bf05670456adfcaa350c9c179374da9da3
mnnam1302 <mnnam.khtn@gmail.com>
2023-11-26 10:40:57 +0700

    feat: add attribute and filter authorized JWT

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,5 @@
-﻿using backend.Models;
+﻿using backend.Filters;
+using backend.Models;
 using backend.Repositories;
 using backend.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -7,13 +8,13 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 
-
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 // DbContext
 builder.Services.AddDbContext<InsuranceDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("InsuranceConnectionString")));
@@ -26,9 +27,9 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ITokenRepository, TokenRepository>();
 builder.Services.AddScoped<ITokenService, TokenService>();
 
-//builder.Services.AddScoped<JwtAuthorizeFilter>();
+// Filter request required Authorized
+builder.Services.AddScoped<JwtAuthorizeFilter>();
 
-// Đăng ký dịch vụ phân quyền
 builder.Services.AddAuthorization();
 
 // JWT
@@ -37,6 +38,7 @@ builder.Services.AddAuthentication(options =>
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 });
+
 
 var app = builder.Build();
 
