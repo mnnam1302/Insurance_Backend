29a504eae79722852f3a39b8536db955e20d9029
hungbin0209 <rename0209@gmail.com>
2023-12-31 21:23:53 +0700

    fix error

--- backend/Controllers/ContractController.cs
+++ backend/Controllers/ContractController.cs
@@ -41,10 +41,10 @@ namespace backend.Controllers
 
 
         [HttpPost]
-        //[JwtAuthorize]
-        public async Task<IActionResult> AddNewContract([FromBody] AddContractDTO dto)
+        [JwtAuthorize]
+        public async Task<IActionResult> AddNewContract([FromBody] AddContractDTO addContractDTO)
         {
-            if (dto == null)
+            if (addContractDTO == null)
             {
                 return BadRequest("Request is not valid");
             }
@@ -53,17 +53,17 @@ namespace backend.Controllers
                 //Kiểm tra người mua có tồn tại không
                 int userId;
 
-                //userId = HttpContext.GetUserId();
-                //var user = await _userService.GetUserById(userId);
+                userId = HttpContext.GetUserId();
+                var user = await _userService.GetUserById(userId);
 
-                //if (user == null)
-                //{
-                //    return NotFound("Policyholder is not found");
-                //}
+                if (user == null)
+                {
+                    return NotFound("Policyholder is not found");
+                }
 
                 var contract_dto = new ContractDTO();
-                contract_dto.user_id = 1;
-                contract_dto.registration_id = dto.registration_id;//userId
+                contract_dto.user_id = userId;
+                contract_dto.registration_id = addContractDTO.registration_id;//userId
 
                 // thêm hợp đồng
                 var result = await _contractService.AddNewContract(contract_dto);
@@ -89,7 +89,7 @@ namespace backend.Controllers
             try
             {
                 // Kiểm tra người mua có tồn tại không
-                int userId;
+                int userId = 1;
 
                 userId = HttpContext.GetUserId();
                 var user = await _userService.GetUserById(userId);
